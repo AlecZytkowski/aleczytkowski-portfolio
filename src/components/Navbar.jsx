17cb445ab2595e38279ce40cdcfd3661c00db951
Dylan Barber <dylan@solustream.com>
2023-07-17 22:01:41 -0400

    working with Alec on this, explained the changes and cleaned some stuff up with him

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,29 @@
 import './Navbar.css';
 
 export const Navbar = () => {
-    const handleHireButtonClick = () => {
-        window.location.href = 'mailto:aleczytkowski@gmail.com';
-      };
-    return (
-        <nav className="navbar">
-            <div className="logo">Alec Zytkowski
-            </div>
-            <ul className="nav-links">
-                <li className="nav-item">
-                    <a href="#home">Home</a>
-                </li>
-                <li className="nav-item">
-                    <a href="#about">About Me</a>
-                </li>
-                <li className="nav-item">
-                  <a href="#projects">Projects</a>
-                </li>
-                <li className="nav-item">
-                  <a href="#contact">Contact</a>
-                </li>
-            </ul>
-            <button className="hire-button" onClick={handleHireButtonClick}>
-            Contact Me!
+  const handleHireButtonClick = () => {
+    window.location.href = 'mailto:aleczytkowski@gmail.com/subject=Hello Alec!';
+  };
+  return (
+    <nav className="navbar">
+      <div className="logo">Alec Zytkowski</div>
+      <ul className="nav-links">
+        <li className="nav-item">
+          <a href="#home">Home</a>
+        </li>
+        <li className="nav-item">
+          <a href="#about">About Me</a>
+        </li>
+        <li className="nav-item">
+          <a href="#projects">Projects</a>
+        </li>
+        <li className="nav-item">
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <button className="hire-button" onClick={handleHireButtonClick}>
+        Contact Me!
       </button>
     </nav>
-    );
+  );
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,70 +3,56 @@ import './Contact.css'
 import { SocialIcon } from 'react-social-icons';
 
 export const Contact = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      window.location.href = 'mailto:aleczytkowski@gmail.com';
-      setName('');
-      setEmail('');
-    };
-  
-    const handleNameChange = (e) => {
-      setName(e.target.value);
-    };
-  
-    const handleEmailChange = (e) => {
-      setEmail(e.target.value);
-    };
-  
-    return (
-      <div className="contact" id="contact">
-        <h1>Contact Me</h1>
-        <div>
+  const [emailData, setEmailData] = useState({ name: '', message: '' });
+
+  const { name, message } = emailData;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    window.location.href = `mailto:aleczytkowski@gmail.com/?subject=Hello Alec!&body=${message}`;
+  };
+
+  const handleInputChange = (e) => setEmailData({ ...emailData, [e.target.name]: e.target.value });
+
+  return (
+    <div className="contact" id="contact">
+      <h1>Contact Me</h1>
+      <div>
         <ul className="social-links">
           <li className="social-item">
-            <SocialIcon url='https://github.com/AlecZytkowski' bgColor='white'/>
+            <SocialIcon url='https://github.com/AlecZytkowski' bgColor='white' />
           </li>
           <li className="social-item">
-            <SocialIcon url="https://www.linkedin.com/in/alec-zytkowski-194719226/" bgColor='white'/>
+            <SocialIcon url="https://www.linkedin.com/in/alec-zytkowski-194719226/" bgColor='white' />
           </li>
         </ul>
-        </div>
-        <div className='contact-form'>
-        <form onSubmit={handleSubmit}>
         <p>Or, you can just send me an Email!</p>
+      </div>
+      <div className='contact-form'>
+        <form onSubmit={handleSubmit}>
           <label className='form-label' htmlFor="name">Name</label>
-          <br />
           <input
             className='form-input'
             type="text"
             id="name"
+            name='name'
             value={name}
-            onChange={handleNameChange}
+            onChange={handleInputChange}
             placeholder="Enter your name"
             required
           />
-          <br />
-          <label className='form-label' htmlFor="email">Email</label>
-          <br />
-          <input
-            className='form-input'
-            type="email"
-            id="email"
-            value={email}
-            onChange={handleEmailChange}
-            placeholder="Enter a valid email"
-            required
+          <label className='form-label' htmlFor="message">Message</label>
+          <textarea 
+            name='message' 
+            onChange={handleInputChange}
+            value={message}
+            className='form-text-area' 
+            id="message" 
+            placeholder="Enter your message here"
           />
-          <br />
-          <label className='form-label' htmlFor="email">Message</label>
-          <textarea  className='form-input' id="message" rows="4" placeholder="Enter your message here"></textarea>
-          <br />
           <button type="submit" className='form-submit-button'>Contact Me!</button>
         </form>
-        </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
