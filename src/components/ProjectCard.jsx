70a96c44285762488b8cbea792570872c0bd9048
Alec Zytkowski <aleczytkowski@gmail.com>
2023-09-21 18:23:35 -0400

    Added Links to Projects!

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,9 +3,13 @@ import { SocialIcon } from 'react-social-icons';
 
 export const ProjectCard = ({ project }) => {
 
-return(
+    const handleLinkClick = () => {
+        window.open(project.websiteUrl, '_blank');
+    };
+
+    return(
         <div className='project-card'>
-            <div>
+            <div onClick={handleLinkClick}>
                 <h1>{project.name}</h1>
                 <p>{project.description}</p>
             </div>
